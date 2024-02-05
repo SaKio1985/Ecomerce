ad13adea86fe11bdddb3f1ff3433d82061241dc0
¨SaKio¨ <sakio@tuta.io>
2024-02-05 22:37:49 +0100

    Primera prueba del ecomerce

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import Navbar from './components/navbar';
+import ProductCard from './components/ProductCard';
+import data from './data.json'; 
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filteredProducts, setFilteredProducts] = useState(data);
+
+  const handleSearchChange = (e) => {
+    const newSearchTerm = e.target.value
+    setSearchTerm(newSearchTerm);
+    filterProducts(newSearchTerm);
+  };
+
+  const filterProducts = (newSearchTerm) => {
+    const filtered = data.filter((product) =>
+      product.title.toLowerCase().includes(newSearchTerm.toLowerCase())
+    );
+    setFilteredProducts(filtered);
+  };
+
+  return (
+    <div>
+      <Navbar onSearchSubmit={filterProducts} />
+      <div className="product-list">
+        {filteredProducts.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
